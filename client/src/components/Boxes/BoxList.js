bdf7beb9d5f5f0e9e70265b318e0005c47a075fa
Chady Ghribi <Chady>
2021-02-10 11:51:27 +0100

    navbar register boxCard

---
+++ client/src/components/Boxes/BoxList.js
@@ -0,0 +1,18 @@
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {getBox} from '../../redux/actions/boxAction';
+import BoxCard from './BoxCard';
+const BoxList =()=>{
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        dispatch(getBox());
+    }, []);
+    const boxs = useSelector((state)=>state.boxs.box);
+
+    return(
+        <div>
+            {boxs && boxs.map((el)=><BoxCard el={el} key = {el._id}/>)}
+        </div>
+    )
+}
+export default BoxList ;
