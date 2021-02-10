a46bdb594e52b19e778debb473201c91ad4df569
Slim Masmoudi <slimmasmoudi@ymail.com>
2021-02-10 12:35:07 +0100

    browserrouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
-import './App.css';
-import BoxList from './components/Boxes/BoxList'
-import NavBar from './components/navBar/NavBar'
-import RegisterSide from './components/RegisterAndLogin/RegisterSide'
+import "./App.css";
+import { BrowserRouter, Link, Route } from "react-router-dom";
+import BoxList from "./components/Boxes/BoxList";
+import NavBar from "./components/navBar/NavBar";
+import RegisterSide from "./components/RegisterAndLogin/RegisterSide";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <NavBar />
-      <BoxList />
-    </div>
+      <div className="App">
+        <Route path="/lunchbox" render={() => <BoxList />} />
+      </div>
+    </BrowserRouter>
   );
 }
 
