ef4301f675500be654cbf38803a366ad6eb6e5ca
Slim Masmoudi <slimmasmoudi@ymail.com>
2021-02-11 13:05:41 +0100

    contactus back & front

--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -1,5 +1,10 @@
-import {combineReducers} from 'redux';
-import {tataReducer} from './tataReducer';
-import {boxReducer} from './boxReducer';
+import { combineReducers } from "redux";
+import { tataReducer } from "./tataReducer";
+import { boxReducer } from "./boxReducer";
+import { ContactUsReducer } from "./ContactUsReducer";
 
-export  const rootReducer= combineReducers({boxReducer,tataReducer})
+export const rootReducer = combineReducers({
+  boxReducer,
+  tataReducer,
+  ContactUsReducer,
+});
