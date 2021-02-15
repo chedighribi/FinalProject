db7f9b9381abab2e43c49cc0e024582a0fa4c772
derouiche-aymen <aymenonea@live.fr>
2021-02-15 10:41:46 +0100

    tata crud

--- client/src/redux/actions/tataAction.js
+++ client/src/redux/actions/tataAction.js
@@ -6,4 +6,26 @@ export const getTata=()=>(dispatch)=>{
   .get('/api/tata' )
   .then((res)=>dispatch({type : GET_TATA, payload:res.data}))
   .catch((err)=> console.log(err))
-}
+};
+
+export const editTata = (id,editedTata) => (dispatch) => {
+  axios
+    .put(`/api/editata/${id}`,editedTata)
+    .then((res) => dispatch(getTata()))
+    .catch((err) => console.log(err));
+};
+
+export const deleteTata = (id)=>(dispatch)=>{
+  axios
+  .delete(`/api/delete/${id}`)
+  .then ((res)=> dispatch(getTata()))
+  .catch((err)=> console.log(err));
+};
+
+
+export const addTata=(newTata)=>(dispatch)=>{
+  axios
+  .post('/api/addTata' , newTata)
+  .then((res)=>dispatch(getTata()))
+  .catch((err)=> console.log(err))
+};
