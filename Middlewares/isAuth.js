cd6dd1b755517986579cc572692c22dceafe6a52
Slim Masmoudi <slimmasmoudi@ymail.com>
2021-02-17 16:04:13 +0100

    spinner loading in app

--- Middlewares/isAuth.js
+++ Middlewares/isAuth.js
@@ -1,24 +1,22 @@
-const jwt = require ('jsonwebtoken');
-const config = require ('config');
-const User = require ('../Models/userModel');
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const User = require("../Models/userModel");
 
-const isAuth = async (req,res,next)=>{
-    try {
-        const token = req.headers['auth-token'];
-        if (!token)
-            return res.status(401).send({msg:'No token'});
+const isAuth = async (req, res, next) => {
+  try {
+    const token = req.headers["auth-token"];
+    if (!token) return res.status(401).send({ msg: "No token" });
+    const decoded = await jwt.verify(token, config.get("SECRETKEY"));
 
-        const decoded = await jwt.verify(token, config.get("SECRETKEY"));
-
-        const Tuser= await User.findById(decoded.id)
-        if (!Tuser){
-            return res.status(401).send({msg:'denied'});
-        }
-        req.user=Tuser;
-        next();
-    } catch (error) {
-        return res.status(400).json({msg:'Token not valid',error})
+    const Tuser = await User.findById(decoded.id);
+    if (!Tuser) {
+      return res.status(401).send({ msg: "denied" });
     }
+    req.user = Tuser;
+    next();
+  } catch (error) {
+    return res.status(400).json({ msg: "Token not valid", error });
+  }
 };
 
 module.exports = isAuth;
--- client/src/App.js
+++ client/src/App.js
@@ -11,17 +11,30 @@ import ContactUs from "./components/ContactUs/ContactUs";
 import Login from "./components/RegisterAndLogin/Login";
 import AddBox from "./components/Boxes/AddBox";
 import { getAuthUser } from "./redux/actions/userAction";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import AddTata from "./components/tatas/AddTata";
+import { Spinner } from "reactstrap";
 
 function App() {
   const dispatch = useDispatch();
   const getUser = () => dispatch(getAuthUser());
+  const { isLoading } = useSelector((state) => state.authReducer);
 
   useEffect(() => {
     getUser();
   }, []);
+
+  if (isLoading) {
+    return (
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <Spinner
+          style={{ width: "3rem", height: "3rem", color: "secondary" }}
+          type="grow"
+        />
+      </div>
+    );
+  }
 
   return (
     <div className="App">
